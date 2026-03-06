afe91148a112f9aff57ded1ea7c734406e2e3ea2
bergamota01 <bernardo.bettega@escola.pr.gov.br>
2026-03-06 20:29:44 -0300

    Add files via upload

---
+++ contassoma.cpp
@@ -0,0 +1,69 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main()
+{
+    double a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v;
+    
+    cout<<"insira o valor1: ";
+    cin>>a;
+   cout<<"insira o valor2: ";
+    cin>>b;
+    cout<<"insira o valor3: ";
+    cin>>c;
+    cout<<"insira o valor4: ";
+    cin>>d;
+    cout<<"insira o valor5: ";
+    cin>>e;
+    cout<<"insira o valor6: ";
+    cin>>f;
+    cout<<"insira o valor7: ";
+    cin>>g;
+    cout<<"insira o valor8: ";
+    cin>>h;
+    cout<<"insira o valor9: ";
+    cin>>i;
+    cout<<"insira o valor10: ";
+    cin>>j;
+    cout<<"insira o valor11: ";
+    cin>>k;
+    cout<<"insira o valor12: ";
+    cin>>l;
+    cout<<"insira o valor13: ";
+    cin>>m;
+    cout<<"insira o valor14: ";
+    cin>>n;
+    cout<<"insira o valor15: ";
+    cin>>o;
+    cout<<"insira o valor16: ";
+    cin>>p;
+    cout<<"insira o valor17: ";
+    cin>>q;
+    cout<<"insira o valor18: ";
+    cin>>r;
+    cout<<"insira o valor19: ";
+    cin>>s;
+    cout<<"insira o valor20: ";
+    cin>>t;
+    cout<<"insira o valor21: ";
+    cin>>u;
+    cout<<"insira o valor22: ";
+    cin>>v;
+    
+    
+   
+    double TOTAL=a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v;
+    cout<<"\nTOTAL: "<<TOTAL;
+   
+  
+
+    return 0;
+}
